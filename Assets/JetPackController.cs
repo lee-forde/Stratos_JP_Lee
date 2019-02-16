8b94bfe2ea7f3218354521fb3201e37a4d156b22
lee-forde <46717325+lee-forde@users.noreply.github.com>
2019-02-16 18:48:50 +0000

    Initial commit

---
+++ Assets/JetPackController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPackController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float jetpackForce = 75.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+        if (jetpackActive)
+        {
+            rb2d.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+}
